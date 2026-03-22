bf6618b972db6eafe9f1172b482b583a17ef72e6
藏令伊 <145777614+LudwigIrisNebula@users.noreply.github.com>
2026-03-23 00:35:19 +0800

    Reset project with Yarn mappings

---
+++ src/main/java/io/github/ludwigirisnebula/ludwigsmysteriousworld/LudwigsMysteriousWorldClient.java
@@ -0,0 +1,10 @@
+package io.github.ludwigirisnebula.ludwigsmysteriousworld;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LudwigsMysteriousWorldClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/io/github/ludwigirisnebula/ludwigsmysteriousworld/LudwigsMysteriousWorldDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.ludwigirisnebula.ludwigsmysteriousworld;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LudwigsMysteriousWorldDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
