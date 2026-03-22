56a7e60067010d41bbdafbea7a3759d4721ff45d
藏令伊 <145777614+LudwigIrisNebula@users.noreply.github.com>
2026-03-23 03:00:35 +0800

    add something

--- src/main/java/io/github/ludwigirisnebula/ludwigsmysteriousworld/item/ModItems.java
+++ src/main/java/io/github/ludwigirisnebula/ludwigsmysteriousworld/item/ModItems.java
@@ -6,6 +6,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
@@ -13,14 +15,35 @@ public class ModItems {
 //    Items
     // Special Items
     public static final Item LUDWIGS_IRIS_PICTURE = registerItem(
-            "ludwigs_iris_picture", new Item(new Item.Settings()));
+            "ludwigs_iris_picture", new Item(new Item.Settings()
+                .registryKey(RegistryKey.of(
+                        RegistryKeys.ITEM,
+                        Identifier.of(LudwigsMysteriousWorld.MOD_ID, "ludwigs_iris_picture")))
+        ));
+
+//    public static final Item LUDWIGS_IRIS_PICTURE = registerItem(
+//            "ludwigs_iris_picture", new Item(new Item.Settings()));
+//  --Original--
 
 
 
 //    Main register
 
-    private  static Item registerItem(String name, Item item){
-        return Registry.register(Registries.ITEM, Identifier.of(LudwigsMysteriousWorld.MOD_ID, name), item);
+    private static Item registerItem(String name, Item item){
+//        --Test--(and worked)
+//        Identifier id = Identifier.of(LudwigsMysteriousWorld.MOD_ID, name);
+//        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+//        Item.Settings settings = new Item.Settings().registryKey(key);
+//        return Registry.register(
+//                Registries.ITEM,
+//                key,
+//                item
+//        );
+        //        --Original--
+        return Registry.register(
+                Registries.ITEM,
+                Identifier.of(LudwigsMysteriousWorld.MOD_ID, name), item);
+
     }
 
     public static void registerModItems(){
@@ -28,8 +51,9 @@ public class ModItems {
                 "Registering Mod Items for " + LudwigsMysteriousWorld.MOD_ID);
 
         //    Add to item group (wait to create my own item group)
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
-            fabricItemGroupEntries.add(LUDWIGS_IRIS_PICTURE);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register(entries -> {
+            entries.add(LUDWIGS_IRIS_PICTURE);
         });
     }
 }
